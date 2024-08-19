9f1d48c3743cb394abf50bdd1dd33de03a699974
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-08-19 15:05:11 +0200

    edit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 1337;
 
+
+
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
@@ -9,3 +11,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
