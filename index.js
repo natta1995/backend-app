83c1b5a72b9174389e6c8b7cb128159965de1ac4
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-08-25 12:06:34 +0200

    installing cors

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 
 const session = require('express-session');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.DB_PORT;
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true, 
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
